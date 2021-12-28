991f4d45dace71f87d2e769935bf0cb1d79e018c
Payne <wuzhipeng1289690157@gmail.com>
2021-12-28 17:44:52 +0800

    fix format

--- DumpMan/script.py
+++ DumpMan/script.py
@@ -11,8 +11,7 @@ from config import ResourceList
 from process import all_entrances
 import json
 from urllib.parse import unquote
-from mitmproxy import ctx
-
+# from mitmproxy import ctx
 # def request(flow):
 #     ctx.log.info(str(flow.request.url))
 #     ctx.log.info(str(flow.request.method))
@@ -26,10 +25,10 @@ def response(flow) -> None:
     """
     for source in ResourceList.keys():
         if ResourceList.get(source) in flow.request.url:
-            ctx.log.error("-----------------------")
-            ctx.log.info(f'Starting Process Filter URLS: {ResourceList.get(source)}')
+            print("-----------------------")
+            print(f'Starting Process Filter URLS: {ResourceList.get(source)}')
             body = unquote(flow.response.text)
             data = json.loads(body)
             pprint.pprint(data)
             results: list[Any] | Any = data['data']['items'] or []
-            all_entrances(source, results)
+            # all_entrances(source, results)
